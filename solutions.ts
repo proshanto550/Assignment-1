641dc60ccd8bf163605e56d73550a630e019bdb2
proshanto550 <proshantodas550@gamil.com>
2026-05-08 03:03:51 +0600

    Solutions for Assignment 1

---
+++ solutions.ts
@@ -0,0 +1,64 @@
+// Problem 1:
+function filterEvenNumbers(numbers: number[]): number[] {
+    const evenNumbers = numbers.filter(num => num % 2 === 0);
+    return evenNumbers;
+}
+
+
+// Problem 2:
+function reverseString(str: string): string {
+    return str.split("").reverse().join("");
+}
+
+
+// Problem 3:
+function checkType(input: string | number): "String" | "Number" {
+    if (typeof input === "string") {
+        return "String";
+    } else {
+        return "Number";
+    }
+}
+
+
+// Problem 4:
+function getProperty<T, K extends keyof T>(obj: T, key: K): T[K] {
+    return obj[key];
+}
+
+
+// Problem 5:
+interface Book {
+    title: string;
+    author: string;
+    publishedYear: number;
+}
+
+function toggleReadStatus(book: Book): Book & { isRead: boolean } {
+    return {
+        ...book,
+        isRead: true
+    };
+}
+
+
+// Problem 6:
+class Person {
+    constructor(public name: string, public age: number) {}
+}
+
+class Student extends Person {
+    constructor(name: string, age: number, public grade: string) {
+        super(name, age);
+    }
+
+    getDetails(): string {
+        return `Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}`;
+    }
+}
+
+
+// Problem 7:
+function getIntersection(arr1: number[], arr2: number[]): number[] {
+    return arr1.filter(item => arr2.includes(item));
+}
